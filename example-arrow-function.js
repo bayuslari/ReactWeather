e8a04f9e1d21fbf6f3578e6e5afb9d20c2a632ee
Bayu Slamet Riyadi <bayuslari@Bayus-MacBook-Pro.local>
2017-03-16 14:05:50 +0700

    First commit

---
+++ example-arrow-function.js
@@ -0,0 +1,9 @@
+var addStatement = (a,b) => {
+  console.log(`add statement result is ${a+b}`);
+}
+
+addStatement(1,3);
+
+var addExpression = (a,b) => a+b;
+
+console.log(addExpression(4,5));
---
+++ example-promise.js
@@ -0,0 +1,43 @@
+// function getTempCallback (location, callback){
+// 	callback(undefined, 78);
+//   callback('City not found');
+// }
+//
+// getTempCallback('Philadelphia', function(err, temp){
+//   if (err) {
+//     console.log('error', err);
+//   } else {
+//     console.log('success', temp);
+//   }
+// });
+//
+// function getTempPromise(location){
+//   return new Promise (function(resolve, reject){
+//     setTimeout(function(){
+//       resolve(79);
+//       reject('City not found');
+//     },1000);
+//   });
+// }
+//
+// getTempPromise('Jogja').then(function(temp){
+//   console.log('Promise success', temp);
+// },function(err){
+//   console.log('Promise error!', err);
+// });
+
+function addPromise (a, b){
+  return new Promise (function (resolve, reject){
+    if (typeof a === 'number' && typeof b === 'number') {
+      resolve(a+b);
+    } else {
+      reject('A & B need to be numbers');
+    }
+  });
+};
+
+addPromise(4,2).then(function(sum){
+  console.log('Promise success and the result is: ', sum);
+},function(err){
+  console.log('Promise error!', err);
+});
