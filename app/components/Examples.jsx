3e3cd43fd2c96c969d07ed2f3bb966e38685c3f1
Bayu Slamet Riyadi <bayu93mx@gmail.com>
2017-10-29 15:25:34 +0700

    styling example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
-    return (
-        <h3>Examples Component</h3>
-    )
+  return (
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Yogyakarta'>Yogyakarta</Link>
+        </li>
+        <li>
+          <Link to='/?location=Pontianak'>Pontianak</Link>
+        </li>
+      </ol>
+    </div>
+  )
 };
 
 module.exports = Examples;
